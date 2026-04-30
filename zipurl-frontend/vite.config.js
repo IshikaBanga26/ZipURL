66cda8746355c07845f1201d0b83175eac938078
IshikaBanga26 <ishikabanga01@gmail.com>
2026-04-30 15:50:08 +0530

    fix production build

--- zipurl-frontend/vite.config.js
+++ zipurl-frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://zipurl-ciac.onrender.com'
-    }
-  }
+  base: '/',
 })
